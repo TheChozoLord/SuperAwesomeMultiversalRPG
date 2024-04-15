736f6e7ed0f1a6ce7b3f883ab6c0606dd865a0a0
Owen Merrill <91266947+TheChozoLord@users.noreply.github.com>
2024-04-15 11:47:43 -0400

    Add files via upload

--- FinalProject.py
+++ FinalProject.py
@@ -17,14 +17,32 @@ class Characters(simpleGE.Sprite):
         self.HP = HP
         self.attack = attack
         self.dodge = dodge
+        self.name = "Sonic"
         
         self.image = (pygame.image.load("sonicWPlaceholder.png"))
         self.setSize(50, 75)
         self.position = (320, 400)
         
 class Enemies(simpleGE.Sprite):
-    def __init__(self, scene):
+    def __init__(self, 
+                 scene, 
+                 HP = 0,
+                 attack = 0,
+                 dodge = 0):
         super().__init__(scene)
+        
+        self.HP = HP
+        self.attack = attack
+        self.dodge = dodge
+        
+        def attack(self, target):
+            hit = random.randint(1,100)
+            if (hit >= target.dodge):
+                damage = random.randint(1, self.attack)
+                target.health -= damage
+                print(f"""{self.name} deals {damage} points of damage to {target.name}.""")
+            else:
+                print(f"""{self.name} misses.""")
         
         self.image = (pygame.image.load("skelenemy.jpg"))
         self.setSize(50, 75)
@@ -38,8 +56,30 @@ class BattleScene(simpleGE.Scene):
         self.Characters = Characters(self)
         self.Enemies = Enemies(self)
         
+        self.lblsonichealth = simpleGE.Label(f"{self.character.health}")
+        
+        def process(self):
+            self.fight()
+            self.Enemies(self.attack)
+        
+        def fight(self):
+            keepGoing = True
+            while keepGoing == True:
+                print(f"""{self.Character.name}: {self.Character.health}""")
+                print(f"""{self.Enemies.name}: {self.Enemies.health}""")
+                input("press enter to attack.")
+                self.Characters.attack(self.Enemies)
+                self.Enemies.attack(self.Characters)
+                if self.Character.health <= 0:
+                    print("You lose.")
+                    keepGoing = False
+                if self.Enemies.health <= 0:
+                    print("You win!")
+                    keepGoing = False
+        
         self.sprites = [self.Characters,
-                        self.Enemies]
+                        self.Enemies,
+                        self.lblsonichealth]
         
 def main():
     battle = BattleScene()
